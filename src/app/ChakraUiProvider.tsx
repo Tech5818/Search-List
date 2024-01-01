1df937166ecd5c7e1c8d752fcdf8768f1cb4a3fd
Tech5818 <pqow5818@gmail.com>
2024-01-01 23:37:51 +0900

    :tada: 프로젝트 생성 및 Chakra UI 세팅

---
+++ src/app/ChakraUiProvider.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from "react";
+
+import { CacheProvider } from "@chakra-ui/next-js";
+import { ChakraProvider } from "@chakra-ui/react";
+
+export const ChakraUiProvider = ({children} : {children: React.ReactNode}) => {
+    return(
+        <CacheProvider>
+            <ChakraProvider resetCSS>
+                {children}
+            </ChakraProvider>
+        </CacheProvider>
+    )
+}
